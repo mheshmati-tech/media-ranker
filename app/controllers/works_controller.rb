5c6a8966ede92709893cb89e13ad0a6c443d6405
Maryam Heshmati <mairheshmati@gmail.com>
2020-05-14 14:14:25 -0700

    generated Works controller, work model, and db:migrate and db:seed data for the Work controller.

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,6 @@
 class WorksController < ApplicationController
+    def index
+        @works = Work.all
+    end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+WORKS_FILE = Rails.root.join('db', 'seed_data', 'works-seeds.csv')
+puts "Loading raw driver data from #{WORKS_FILE}"
+
+works_failure = []
+CSV.foreach(WORKS_FILE, :headers => true) do |row|
+  work = Work.new
+  work.category = row["category"]
+  work.title = row["title"]
+  work.creator = row["creator"]
+  work.publication_year = row["publication_year"]
+  work.description = row["description"]
+  successful = work.save
+  if !successful
+    works_failure << driver
+    puts "Failed to save work: #{work.inspect}"
+  else
+    puts "Created work: #{work.inspect}"
+  end
+end
+
+puts "Added #{Work.count} work records"
+puts "#{works_failure.length} works failed to save:("
