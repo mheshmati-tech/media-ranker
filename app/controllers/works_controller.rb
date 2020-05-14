1b9d19b5da87bb8da680fcd84580a34d349772a5
Maryam Heshmati <mairheshmati@gmail.com>
2020-05-14 15:07:33 -0700

    added show action and render the detail for each work

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,8 +1,16 @@
 class WorksController < ApplicationController
-    def index
-        @albums = Work.all.where category: "album"
-        @books = Work.all.where category: "book"
-        @movies = Work.all.where category: "movies"
-    end
+  def index
+    @albums = Work.all.where category: "album"
+    @books = Work.all.where category: "book"
+    @movies = Work.all.where category: "movies"
+  end
 
+  def show
+    @work = Work.find_by(id: params[:id])
+
+    if @work.nil?
+      head :not_found
+      return
+    end
+  end
 end
