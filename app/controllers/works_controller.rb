ed2edc231c9755cfcdf2b9872392e3fcc4576de5
Maryam Heshmati <mairheshmati@gmail.com>
2020-05-14 16:03:25 -0700

    added the update action for Work controller

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -2,7 +2,7 @@ class WorksController < ApplicationController
   def index
     @albums = Work.all.where category: "album"
     @books = Work.all.where category: "book"
-    @movies = Work.all.where category: "movies"
+    @movies = Work.all.where category: "movie"
   end
 
   def show
@@ -30,8 +30,25 @@ class WorksController < ApplicationController
   end
 
   def update
+    @work = Work.find_by(id: params[:id])
+    if @work.nil?
+      head :not_found
+    elsif @work.update(work_params)
+      redirect_to work_path(@work.id)
+      return
+    else
+      render :edit
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def work_params
+    variable = params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+    variable[:category].downcase!
+    return variable
   end
 end
