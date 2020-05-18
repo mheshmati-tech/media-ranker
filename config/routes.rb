865ef5436add1aeaa392137384e7fb1ec4234ee7
Maryam Heshmati <mairheshmati@gmail.com>
2020-05-18 14:35:26 -0700

    updated the routes for login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   root to: 'homepages#index'
   
+  get "/login", to: "users#login_form", as: "login"
+  post "/login", to: "users#login"
+  post "/logout", to: "users#logout", as: "logout"
+  get "/users/current", to: "users#current", as: "current_user"
+
   resources :works
   resources :homepages, only: [:index]
+
 end
