f250dd5b20245e40b8d2744d429151ab211aba62
Maryam Heshmati <mairheshmati@gmail.com>
2020-05-20 12:14:13 -0700

    added more class method to the work model so that the top ten lists reflect on media that have been voted on

--- app/models/work.rb
+++ app/models/work.rb
@@ -7,12 +7,24 @@ class Work < ApplicationRecord
     return Work.where(category: "album").sort_by { |album| album.votes.count }.reverse
   end
 
+  def self.top_ten_albums
+    return Work.albums.select { |album| album.votes.count > 0 }
+  end
+
   def self.books
     return Work.where(category: "book").sort_by { |movie| movie.votes.count }.reverse
   end
 
+  def self.top_ten_books
+    return Work.books.select { |book| book.votes.count > 0 }
+  end
+
   def self.movies
     return Work.where(category: "movie").sort_by { |book| book.votes.count }.reverse
+  end
+
+  def self.top_ten_movies
+    return Work.movies.select { |movie| movie.votes.count > 0 }
   end
 
   validates :title, presence: true
