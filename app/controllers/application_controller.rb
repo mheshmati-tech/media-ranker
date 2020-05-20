13faab7c2a4808356e6d4ed768538617d3d575cc
Maryam Heshmati <mairheshmati@gmail.com>
2020-05-20 14:03:32 -0700

    fixed nil references for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    def look_up_user
+        if session[:user_id]
+            @user = User.find_by(id: session[:user_id])
+        else
+            @user = nil 
+        end
+    end
+    before_action :look_up_user
+
 end
